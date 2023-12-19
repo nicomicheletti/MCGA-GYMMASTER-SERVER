751db38ed3313f5050a7bba5b0f6ea01fc097715
Francisco Bruno <franbruno99@gmail.com>
2023-12-19 01:58:37 -0300

    express http error handling

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import gymClassesRoutes from "./routes/gymClasses";
 import morgan from "morgan";
+import createHttpError, {isHttpError} from "http-errors";
 
 const app = express();
 
@@ -12,15 +13,17 @@ app.use(express.json());
 app.use("/api/gymclasses", gymClassesRoutes);
 
 app.use((req, res, next) => {
-    next(Error("Endpoint not found"));
+    next(createHttpError(404, "Endpoint not found"));
 });
 
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     let errorMessage = "Error: " + error;
-        if (error instanceof Error) {
-            errorMessage = error.message;
-        }
-        res.status(500).json({ error: errorMessage });
+    let statusCode = 500;
+    if(isHttpError(error)) {
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
+    res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
--- src/controllers/gymClasses.ts
+++ src/controllers/gymClasses.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import GymClassModel from "../models/gymClasses";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 export const getGymClasses: RequestHandler = async (req, res, next) => {
     try {
@@ -12,20 +14,39 @@ export const getGymClasses: RequestHandler = async (req, res, next) => {
 
 export const getGymClass: RequestHandler = async (req, res, next) => {
     const gymClassId = req.params.gymClassId;
-
+    
     try {
+        if (!mongoose.isValidObjectId(gymClassId)){
+            throw createHttpError(400, "Invalid gym class id");
+        }
+
         const gymClass = await GymClassModel.findById(gymClassId).exec();
+
+        if(!gymClass) {
+            throw createHttpError(404, "Gym class not found");
+        }
+
         res.status(200).json(gymClass);
+
     } catch (error) {
         next(error);
     }
 };
 
-export const createGymClass: RequestHandler = async (req, res, next) => {
+interface CreateGymClassBody {
+    title?: string;
+    text?: string;
+}
+
+export const createGymClass: RequestHandler<unknown, unknown, CreateGymClassBody, unknown> = async (req, res, next) => {
     const title = req.body.title;
     const text = req.body.text;
 
     try {
+        if(!title) {
+            throw createHttpError(400, "Title is required");
+        }
+        
         const newGymClass = await GymClassModel.create({ 
             title: title,
             text: text,
