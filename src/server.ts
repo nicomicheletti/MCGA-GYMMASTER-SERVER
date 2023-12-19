64bfc4ffe4840ae97ec4d55133e8ef5931cd57f8
Nicolas Micheletti <nicolasamicheletti@gmail.com>
2023-12-18 23:46:07 -0300

    db connected and code ordering

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
-import express from "express";
-const app = express();
-const port = 5000;
+import app from "./app";
+import env from "./util/validateEnv";
+import mongoose from "mongoose";
 
-app.get("/", (req, res) => {
-    res.send("Test port");
-});
+const port = env.PORT;
 
-app.listen(port, () => {
-    console.log("Server running on port: " + port);
-});
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+    .then(() => {
+        console.log("Mongoose connected");
+        app.listen(port, () => {
+            console.log("Server running on port: " + port);
+        });
+    })
+    .catch(console.error);
+
